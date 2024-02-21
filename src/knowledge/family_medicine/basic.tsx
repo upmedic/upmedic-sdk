a0369e5aa05a29366872ee0feda4efc090fdc519
Pawel Paczuski <pawel.pacczuski@upmedic.io>
2024-02-21 15:40:00 +0100

    fix: access node fields

--- src/knowledge/family_medicine/basic.tsx
+++ src/knowledge/family_medicine/basic.tsx
@@ -2,8 +2,8 @@ import { ExpertEngine } from "../../engine";
 
 
 function calculateBMI (reportJson:any)  {
-    const weight = ExpertEngine.getNodeByConstId('weight');
-    const height = ExpertEngine.getNodeByConstId('height');
+    const weight = parseFloat(ExpertEngine.getNodeByConstId('weight').data.text);
+    const height = parseFloat(ExpertEngine.getNodeByConstId('height').data.text);
 
     console.log('calculating BMI and inserting it into target node in the template.')
     ExpertEngine.setNumberNodeValue('BMI', weight/(height*height));
