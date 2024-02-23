bb8376d48d76a83630d3addf5ff060020ce53e27
Pawel Paczuski <pawel.pacczuski@upmedic.io>
2024-02-23 07:52:20 +0100

    chore: prettier config + reformat

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ExpertEngine } from './engine';
-import './knowledge/knowledgeBase' 
+import './knowledge/knowledgeBase';
 
 function App() {
   return (
     <>
-    <div className="App">
-      <h1>
-        upmedic medical knowledge base engine simulator.
-      </h1>
-      <p><a href='https://developer.chrome.com/docs/devtools/console/' target='blank'>Open devtools</a> console log to see results of analysis</p>
-      <button onClick={(e) => ExpertEngine.execute()}> RUN ANALYSIS</button>
-    </div>
-    <div>
-      <h2>Requirements status for the template and report:</h2>
-      {JSON.stringify(ExpertEngine.checkRequirements(), null, 2)}
-      <h3>Debug info for requirements:</h3>
-      <pre>{ExpertEngine.registeredCalculations.map(c=>JSON.stringify(ExpertEngine.checkRequirementsForCalculation(c), null, 2))}</pre>
-    </div>
-    <div>
-      <h2>Report (can be modified in simulationData/report.json)</h2>
+      <div className='App'>
+        <h1>upmedic medical knowledge base engine simulator.</h1>
+        <p>
+          <a
+            href='https://developer.chrome.com/docs/devtools/console/'
+            target='blank'
+          >
+            Open devtools
+          </a>{' '}
+          console log to see results of analysis
+        </p>
+        <button onClick={(e) => ExpertEngine.execute()}> RUN ANALYSIS</button>
+      </div>
+      <div>
+        <h2>Requirements status for the template and report:</h2>
+        {JSON.stringify(ExpertEngine.checkRequirements(), null, 2)}
+        <h3>Debug info for requirements:</h3>
+        <pre>
+          {ExpertEngine.registeredCalculations.map((c) =>
+            JSON.stringify(ExpertEngine.checkRequirementsForCalculation(c), null, 2),
+          )}
+        </pre>
+      </div>
+      <div>
+        <h2>Report (can be modified in simulationData/report.json)</h2>
         <pre>{JSON.stringify(ExpertEngine.report, null, 2)}</pre>
-    </div>
-    <div>
-      <h2>Template (can be modified in simulationData/template.json)</h2>
+      </div>
+      <div>
+        <h2>Template (can be modified in simulationData/template.json)</h2>
         <pre>{JSON.stringify(ExpertEngine.template, null, 2)}</pre>
-    </div>
-
+      </div>
     </>
   );
 }
--- src/knowledge/knowledgeBase.tsx
+++ src/knowledge/knowledgeBase.tsx
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import './family_medicine/BMI' 
-import './family_medicine/incorrectBMI' 
-console.log('Knowledge base loaded')
+import './family_medicine/BMI';
+import './family_medicine/incorrectBMI';
+console.log('Knowledge base loaded');
