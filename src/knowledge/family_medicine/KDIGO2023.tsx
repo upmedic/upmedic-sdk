4aae3cb8fd593b4b85ceb7c2660e0d04ea5cdc0d
Przemysław Bombiński <przemyslaw.bombinski@upmedic.io>
2024-03-14 17:46:25 +0100

    code optimization

--- src/knowledge/family_medicine/KDIGO2023.tsx
+++ src/knowledge/family_medicine/KDIGO2023.tsx
@@ -41,93 +41,68 @@ const KDIGO2023: EngineCalculation = {
     );
     // eGFR evaluation
     let egfr_cat: string = ''; // eGFR category - G1-G5
+    let egfr_cat_comment: string = ''; // adds comment to eGFR category
     if (egfr >= 90) {
       egfr_cat = 'G1';
-      ExpertEngine.Report.addToConclusions(
-        `eGFR = ${egfr}`,
-        `kategoria ${egfr_cat} - prawidłowy lub zwiększony`,
-      );
+      egfr_cat_comment = 'prawidłowy lub zwiększony';
     } else if (egfr > 60 && egfr < 89) {
       egfr_cat = 'G2';
-      ExpertEngine.Report.addToConclusions(
-        `eGFR = ${egfr}`,
-        `kategoria ${egfr_cat} - nieznacznie zmniejszony`,
-      );
+      egfr_cat_comment = 'nieznacznie zmniejszony';
     } else if (egfr > 45 && egfr < 59) {
       egfr_cat = 'G3a';
-      ExpertEngine.Report.addToConclusions(
-        `eGFR = ${egfr}`,
-        `kategoria ${egfr_cat} - nieznacznie do umiarkowanie zmniejszony`,
-      );
+      egfr_cat_comment = 'nieznacznie do umiarkowanie zmniejszony';
     } else if (egfr > 30 && egfr < 44) {
       egfr_cat = 'G3b';
-      ExpertEngine.Report.addToConclusions(
-        `eGFR = ${egfr}`,
-        `kategoria ${egfr_cat} - umiarkowanie do znacznie zmniejszony`,
-      );
+      egfr_cat_comment = 'umiarkowanie do znacznie zmniejszony';
     } else if (egfr >= 15 && egfr < 29) {
       egfr_cat = 'G4';
-      ExpertEngine.Report.addToConclusions(
-        `eGFR = ${egfr}`,
-        `kategoria ${egfr_cat} - znacznie zmniejszony`,
-      );
+      egfr_cat_comment = 'znacznie zmniejszony';
     } else {
       egfr_cat = 'G5';
-      ExpertEngine.Report.addToConclusions(
-        `eGFR = ${egfr}`,
-        `kategoria ${egfr_cat} - niewydolność nerek (sprawdź, czy wpisałeś wartość eGFR)`,
-        // check if value was given - value "0" is default
-      );
+      egfr_cat_comment = 'niewydolność nerek (sprawdź, czy wpisałeś wartość eGFR)';
+      // check if value was given - value "0" is default
     }
+    ExpertEngine.Report.addToConclusions(
+      `eGFR = ${egfr}`,
+      `kategoria ${egfr_cat} - ${egfr_cat_comment}`,
+    );
     // albumin in urine evaluation
     let albumin_cat: string = ''; // albumin category - A1-A3
+    let albumin_cat_comment: string = ''; // albumin category comment
     // add mg/mmol with "or" condition
     if (albumin_mg_g < 30) {
       albumin_cat = 'A1';
-      ExpertEngine.Report.addToConclusions(
-        `albumina w moczu = ${albumin_mg_g}`,
-        `kategoria ${albumin_cat} - prawidłowa do nieznacznie zwiększonej (sprawdź, czy wpisałeś wartość albumin)`,
-        // check if value was given - value "0" is default
-      );
+      albumin_cat_comment =
+        'prawidłowa do nieznacznie zwiększonej (sprawdź, czy wpisałeś wartość albumin)';
     } else if (albumin_mg_g >= 30 && albumin_mg_g <= 299) {
       albumin_cat = 'A2';
-      ExpertEngine.Report.addToConclusions(
-        `albumina w moczu = ${albumin_mg_g}`,
-        `kategoria ${albumin_cat} - umiarkowanie zwiększona`,
-      );
+      albumin_cat_comment = 'umiarkowanie zwiększona';
     } else {
       albumin_cat = 'A3';
-      ExpertEngine.Report.addToConclusions(
-        `albumina w moczu = ${albumin_mg_g}`,
-        `kategoria ${albumin_cat} - znacznie zwiększona`,
-      );
+      albumin_cat_comment = 'znacznie zwiększona';
     }
+    ExpertEngine.Report.addToConclusions(
+      `albumina w moczu = ${albumin_mg_g}`,
+      `kategoria ${albumin_cat} - ${albumin_cat_comment}`,
+    );
     // risk level evaluation based on eGFR and albumin categories
     let risk_level: string = '';
+    let risk_recommendation: string = ''; // recommendation for doctor
     if ((egfr_cat === 'G1' || egfr_cat === 'G2') && albumin_cat === 'A1') {
       risk_level = 'małe ryzyko';
-      ExpertEngine.Report.addToConclusions(
-        `Ryzyko progresji PChN - ${risk_level}`,
-        `zalecenia - monitorować 1x/rok`,
-      );
+      risk_recommendation = 'monitorować 1x/rok';
     } else if (
       (egfr_cat === 'G3a' && albumin_cat === 'A1') ||
       ((egfr_cat === 'G1' || egfr_cat === 'G2') && albumin_cat === 'A2')
     ) {
       risk_level = 'umiarkowanie zwiększone ryzyko';
-      ExpertEngine.Report.addToConclusions(
-        `Ryzyko progresji PChN - ${risk_level}`,
-        `zalecenia - leczyć i monitorować 1x/rok`,
-      );
+      risk_recommendation = 'leczyć i monitorować 1x/rok';
     } else if (
       (egfr_cat === 'G3b' && albumin_cat === 'A1') ||
       (egfr_cat === 'G3a' && albumin_cat === 'A2')
     ) {
       risk_level = 'duże ryzyko';
-      ExpertEngine.Report.addToConclusions(
-        `Ryzyko progresji PChN - ${risk_level}`,
-        `zalecenia - leczyć i monitorować 2x/rok`,
-      );
+      risk_recommendation = 'leczyć i monitorować 2x/rok';
     } else if (
       ((egfr_cat === 'G1' ||
         egfr_cat === 'G2' ||
@@ -137,22 +112,20 @@ const KDIGO2023: EngineCalculation = {
       ((egfr_cat === 'G3b' || egfr_cat === 'G4') && albumin_cat === 'A2') ||
       (egfr_cat === 'G4' && albumin_cat === 'A1')
     ) {
-      risk_level = 'bardzo duże ryzyko 1';
-      ExpertEngine.Report.addToConclusions(
-        `Ryzyko progresji PChN - ${risk_level}`,
-        `zalecenia - leczyć, monitorować 3x/rok i skierować do nefrologa`,
-      );
+      risk_level = 'bardzo duże ryzyko I';
+      risk_recommendation = 'leczyć, monitorować 3x/rok i skierować do nefrologa';
     } else if (
       (egfr_cat === 'G4' && albumin_cat === 'A3') ||
       (egfr_cat === 'G5' &&
         (albumin_cat === 'A1' || albumin_cat === 'A2' || albumin_cat === 'A3'))
     ) {
-      risk_level = 'bardzo duże ryzyko 2';
-      ExpertEngine.Report.addToConclusions(
-        `Ryzyko progresji PChN - ${risk_level}`,
-        `zalecenia - leczyć, monitorować 4x/rok i skierować do nefrologa`,
-      );
+      risk_level = 'bardzo duże ryzyko II';
+      risk_recommendation = 'leczyć, monitorować 4x/rok i skierować do nefrologa';
     }
+    ExpertEngine.Report.addToConclusions(
+      `Ryzyko progresji PChN - ${risk_level}`,
+      `zalecenia - ${risk_recommendation}`,
+    );
   },
 };
 ExpertEngine.register(KDIGO2023);
